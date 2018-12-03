e889a3ffa1e14062e88405e8396ff2c326aa683d
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-03 16:00:16 -0600

    added test for mineBlock

--- block.js
+++ block.js
@@ -1,9 +1,23 @@
+const { GENESIS_DATA } = require('./config')
+
 class Block {
   constructor ({ timestamp, lasthash, hash, data }) {
     this.timestamp = timestamp
     this.lasthash = lasthash
     this.hash = hash
     this.data = data
+  }
+
+  static genesis () {
+    return new this(GENESIS_DATA)
+  }
+
+  static mineBlock ({ lastBlock, data }) {
+    return new this({
+      timestamp: Date.now(),
+      lasthash: lastBlock.hash,
+      data
+    })
   }
 }
 module.exports = Block
---
+++ config.js
@@ -0,0 +1,7 @@
+const GENESIS_DATA = {
+  timestamp: 1,
+  lasthash: '---------',
+  hash: 'hash-one',
+  data: []
+}
+module.exports = { GENESIS_DATA }
