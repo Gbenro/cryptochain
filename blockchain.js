7a402d6f27e53ebd0804027a21c37c3e3b879d06
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-03 18:26:23 -0600

    added test for Blockchain

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,6 @@
 const Block = require('./block')
+const cryptoHash = require('./crypto-hash')
+
 class Blockchain {
   constructor () {
     this.chain = [Block.genesis()]
@@ -10,6 +12,37 @@ class Blockchain {
       data
     })
     this.chain.push(newBlock)
+  }
+
+  replaceChain (chain) {
+    if (chain.length <= this.chain.lenght) {
+      console.error('The incoming chain must be longer')
+      return
+    }
+
+    if (!Blockchain.isValidChain(chain)) {
+      console.error('The incoming chain must be valid')
+      return
+    }
+    console.log('Replacing chain with', chain)
+    this.chain = chain
+  }
+
+  static isValidChain (chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+      return false
+    }
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lasthash, hash, data } = chain[i]
+
+      const actualLastHash = chain[i - 1].hash
+
+      if (lasthash !== actualLastHash) return false
+
+      const validatedHash = cryptoHash(timestamp, lasthash, data)
+      if (hash !== validatedHash) return false
+    }
+    return true
   }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,13 @@ const Blockchain = require('./blockchain')
 const Block = require('./block')
 
 describe('Blockchain', () => {
-  const blockchain = new Blockchain()
+  let blockchain, newChain, originalChain
+
+  beforeEach(() => {
+    blockchain = new Blockchain()
+    newChain = new Blockchain()
+    originalChain = blockchain.chain
+  })
 
   it('contains a `chain` Array instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true)
@@ -17,5 +23,73 @@ describe('Blockchain', () => {
     blockchain.addBlock({ data: newData })
 
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData)
+  })
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('return false', () => {
+        blockchain.chain[0] = { data: 'fake-genesis' }
+        expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+      })
+    })
+    describe('when the chain starts with the genesis block and has multiple Blocks', () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: 'Bears' })
+        blockchain.addBlock({ data: 'Bites' })
+        blockchain.addBlock({ data: 'Beerings' })
+      })
+      describe('and lastHash reference has changed', () => {
+        it('returns false', () => {
+          blockchain.chain[2].lasthash = 'broken-lasthash'
+
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+        })
+      })
+
+      describe('and the chain contains a block with an invalid field', () => {
+        it('returns false', () => {
+          blockchain.chain[2].data = 'some-bad-and-evil-data'
+          expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
+        })
+        describe('and the chain does not contains any invalid block', () => {
+          it('returns true', () => {
+            expect(Blockchain.isValidChain(blockchain.chain)).toBe(true)
+          })
+        })
+      })
+    })
+  })
+
+  describe('replaceChain', () => {
+    describe('when the new chain is not longer', () => {
+      it('does not replace the chain', () => {
+        newChain.chain[0] = { new: 'chain' }
+
+        blockchain.replaceChain(newChain.chain)
+
+        expect(blockchain.chain).toEqual(originalChain)
+      })
+    })
+
+    describe('when the new chain is longer', () => {
+      beforeEach(() => {
+        newChain.addBlock({ data: 'Bears' })
+        newChain.addBlock({ data: 'Bites' })
+        newChain.addBlock({ data: 'Beerings' })
+      })
+      describe('and the chain is invalid', () => {
+        it('does not replace the chain', () => {
+          newChain.chain[2].hash = 'some-fake-hash'
+          blockchain.replaceChain(newChain.chain)
+          expect(blockchain.chain).toEqual(originalChain)
+        })
+      })
+      describe('and the chain is valid ', () => {
+        it('replaces the chain', () => {
+          blockchain.replaceChain(newChain.chain)
+          expect(blockchain.chain).toEqual(newChain.chain)
+        })
+      })
+    })
   })
 })
