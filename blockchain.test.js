86a0c098711e56e0492932a46320977c48d61e83
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-03 18:37:13 -0600

    added logMock and ErrorMock for Blockchain test

--- blockchain.test.js
+++ blockchain.test.js
@@ -61,13 +61,26 @@ describe('Blockchain', () => {
   })
 
   describe('replaceChain', () => {
+    let errorMock, logMock
+    beforeEach(() => {
+      errorMock = jest.fn()
+      logMock = jest.fn()
+      global.console.error = errorMock
+      global.console.log = logMock
+    })
+
     describe('when the new chain is not longer', () => {
-      it('does not replace the chain', () => {
+      beforeEach(() => {
         newChain.chain[0] = { new: 'chain' }
 
         blockchain.replaceChain(newChain.chain)
-
+      })
+      it('does not replace the chain', () => {
         expect(blockchain.chain).toEqual(originalChain)
+      })
+
+      it('logs an error', () => {
+        expect(errorMock).toHaveBeenCalled()
       })
     })
 
@@ -78,16 +91,26 @@ describe('Blockchain', () => {
         newChain.addBlock({ data: 'Beerings' })
       })
       describe('and the chain is invalid', () => {
-        it('does not replace the chain', () => {
+        beforeEach(() => {
           newChain.chain[2].hash = 'some-fake-hash'
           blockchain.replaceChain(newChain.chain)
+        })
+        it('does not replace the chain', () => {
           expect(blockchain.chain).toEqual(originalChain)
+        })
+        it('logs an error', () => {
+          expect(errorMock).toHaveBeenCalled()
         })
       })
       describe('and the chain is valid ', () => {
-        it('replaces the chain', () => {
+        beforeEach(() => {
           blockchain.replaceChain(newChain.chain)
+        })
+        it('replaces the chain', () => {
           expect(blockchain.chain).toEqual(newChain.chain)
+        })
+        it('logs about chain replacement', () => {
+          expect(logMock).toHaveBeenCalled()
         })
       })
     })
