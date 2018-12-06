b492d9e5e08add2dafdbbb2fcd62c8acc8f1b687
Gbenro Adesoye <gbenro_adesoye@yahoo.com>
2018-12-05 18:53:21 -0600

    put files into directory

--- app/pubsub.js
+++ app/pubsub.js
@@ -13,9 +13,9 @@ const CHANNELS = {
 }
 
 class PubSub {
-  constructor ({ blockchain, transactionPool }) {
+  constructor ({ blockchain }) {
     this.blockchain = blockchain
-    this.transactionPool = transactionPool
+    // this.transactionPool = transactionPool
 
     this.pubnub = new PubNub(credentials)
 
@@ -31,12 +31,12 @@ class PubSub {
     })
   }
 
-  broadcastTransaction (transaction) {
-    this.publish({
-      channel: CHANNELS.TRANSACTION,
-      message: JSON.stringify(transaction)
-    })
-  }
+  // broadcastTransaction (transaction) {
+  //   this.publish({
+  //     channel: CHANNELS.TRANSACTION,
+  //     message: JSON.stringify(transaction)
+  //   })
+  // }
 
   subscribeToChannels () {
     this.pubnub.subscribe({
@@ -54,19 +54,23 @@ class PubSub {
         )
         const parsedMessage = JSON.parse(message)
 
-        switch (channel) {
-          case CHANNELS.BLOCKCHAIN:
-            this.blockchain.replaceChain(parsedMessage, true, () => {
-              this.transactionPool.clearBlockchainTransactions({
-                chain: parsedMessage.chain
-              })
-            })
-            break
-          case CHANNELS.TRANSACTION:
-            this.transactionPool.setTransaction(parsedMessage)
-            break
-          default:
+        if (channel === CHANNELS.BLOCKCHAIN) {
+          this.blockchain.replaceChain(parsedMessage)
         }
+
+        // switch (channel) {
+        //   case CHANNELS.BLOCKCHAIN:
+        //     this.blockchain.replaceChain(parsedMessage, true, () => {
+        //       this.transactionPool.clearBlockchainTransactions({
+        //         chain: parsedMessage.chain
+        //       })
+        //     })
+        //     break
+        //   case CHANNELS.TRANSACTION:
+        //     this.transactionPool.setTransaction(parsedMessage)
+        //     break
+        //   default:
+        // }
       }
     }
   }
